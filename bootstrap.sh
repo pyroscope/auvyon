b0cc54a6793333dd2364c496b5dd947f19d6345f
pyroscope <pyroscope.project@gmail.com>
2012-04-13 04:06:18 +0200

    installation of new dependencies for spectrograms

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,16 @@
 # This script has to be sourced in a shell and is thus NOT executable.
 
-# Preconditions
-which virtualenv >/dev/null || \
-    { echo "ERROR: You MUST install Python virtualenv (e.g. 'apt-get install python-virtualenv')!"; exit 1; }
-which montage >/dev/null || \
-    { echo "ERROR: You MUST install ImageMagick (e.g. 'apt-get install imagemagick')!"; exit 1; }
-which ffmpeg >/dev/null || \
-    { echo "ERROR: You MUST install ffmpeg (e.g. 'apt-get install ffmpeg')!"; exit 1; }
-which flac >/dev/null || \
-    { echo "ERROR: You MUST install FLAC support (e.g. 'apt-get install flac')!"; exit 1; }
-
-test -f /usr/include/mpg123.h || \
-    { echo "ERROR: You MUST install mpg123 headers (e.g. 'apt-get install libmpg123-dev')!"; exit 1; }
-test -f /usr/include/sndfile.h || \
-    { echo "ERROR: You MUST install sndfile headers (e.g. 'apt-get install libsndfile1-dev')!"; exit 1; }
+# Helpers
+have_pypkg() {
+    ./bin/python -c "import $1" 2>/dev/null
+}
+need_pypkg() {
+    have_pypkg "$1" && false || true
+}
 
 # Use or create virtualenv
+which virtualenv >/dev/null || \
+    { echo "ERROR: You MUST install Python virtualenv (e.g. 'apt-get install python-virtualenv')!"; return 1; }
 if test -f ../bin/activate; then
     test -L bin -a -f bin/activate || ln -nfs ../bin .
 elif test ! -f ./bin/activate; then
@@ -26,21 +21,50 @@ fi
 export DEBFULLNAME=pyroscope
 export DEBEMAIL=pyroscope.project@gmail.com
 
-grep DEBFULLNAME bin/activate || cat >>bin/activate <<EOF
+grep DEBFULLNAME bin/activate >/dev/null || cat >>bin/activate <<EOF
 export DEBFULLNAME=$DEBFULLNAME
 export DEBEMAIL=$DEBEMAIL
 EOF
 
 . bin/activate || return 1
 
+# Preconditions
+ok=true
+which montage >/dev/null || \
+    { echo "ERROR: You MUST install ImageMagick (e.g. 'apt-get install imagemagick')!"; ok=false; }
+which ffmpeg >/dev/null || \
+    { echo "ERROR: You MUST install ffmpeg (e.g. 'apt-get install ffmpeg')!"; ok=false; }
+which flac >/dev/null || \
+    { echo "ERROR: You MUST install FLAC support (e.g. 'apt-get install flac')!"; ok=false; }
+
+# PyWaveform
+have_pypkg waveform || test -f /usr/include/mpg123.h || \
+    { echo "ERROR: You MUST install mpg123 headers (e.g. 'apt-get install libmpg123-dev')!"; ok=false; }
+have_pypkg waveform || test -f /usr/include/sndfile.h || \
+    { echo "ERROR: You MUST install sndfile headers (e.g. 'apt-get install libsndfile1-dev')!"; ok=false; }
+
+# numpy / scipy
+have_pypkg scipy || which gfortran >/dev/null || \
+    { echo "ERROR: You MUST install GNU fortran (e.g. 'apt-get install gfortran')!"; ok=false; }
+have_pypkg numpy || test -d /usr/include/atlas || \
+    { echo "ERROR: You MUST install ATLAS headers (e.g. 'apt-get install libatlas-dev')!"; ok=false; }
+have_pypkg numpy || test -f /usr/lib/liblapack.a || \
+    { echo "ERROR: You MUST install the LAPACK libraries (e.g. 'apt-get install liblapack-dev')!"; ok=false; }
+
+$ok || { echo "FATAL: Fix above dependency errors first!"; return 1; }
+
 # Tools
 ./bin/easy_install -U "setuptools>=0.6c11" || return 1
 ./bin/easy_install -U "paver>=1.0.1" || return 1
 ./bin/easy_install -U "nose>=1.0" || return 1
 ./bin/easy_install -U "yolk>=0.4.1" || return 1
+./bin/easy_install -U "bpython" || return 1
 
 # Dependencies
-easy_install -U https://github.com/superjoe30/PyWaveform/zipball/master
+have_pypkg waveform || easy_install -U https://github.com/superjoe30/PyWaveform/zipball/master || :
+have_pypkg numpy || easy_install numpy || :
+have_pypkg matplotlib || easy_install matplotlib || :
+have_pypkg scipy || easy_install scipy || :
 
 ./bin/paver develop -U
 
